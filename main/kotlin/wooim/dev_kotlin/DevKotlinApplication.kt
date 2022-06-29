7de62e756ce13accf4214343b0033ae378afd96c
wooim <wooim.choi@gmail.com>
2022-06-30 00:37:23 +0900

    kotlin project 설정

---
+++ main/kotlin/wooim/dev_kotlin/DevKotlinApplication.kt
@@ -0,0 +1,11 @@
+package wooim.dev_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevKotlinApplication>(*args)
+}
---
+++ test/kotlin/wooim/dev_kotlin/DevKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package wooim.dev_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
